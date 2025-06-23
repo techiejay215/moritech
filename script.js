4011278763e1ab4265c4d3e446ddc2f60694eedf
techiejay215 <jamesmuriithi252@gmail.com>
2025-06-23 15:07:10 +0300

    Update API base URL to use live backend

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Service Functions with Backend Integration
-const API_BASE_URL = 'http://127.0.0.1:5000/api';
+const API_BASE_URL = 'https://moritech.onrender.com/api';
 
 // Global cart instance
 let cartInstance = null;
